8620f0480a514b7d133c4454879f8dc9434e2060
Aimee Bachari <aimeebachari@gmail.com>
2016-11-30 16:20:41 -0500

    news aggregator

---
+++ server.rb
@@ -0,0 +1,43 @@
+require 'sinatra'
+require 'pry'
+require 'csv'
+
+set :bind, '0.0.0.0'
+
+get '/articles' do
+  erb :articlelist
+end
+
+post '/articles/new' do
+  @title = params[:title]
+  @url = params[:url]
+  @description = params[:description]
+  @errors = []
+
+  if @title == ""
+    @errors << "You have provided an invalid title."
+  end
+
+  if !@url.include?(".") || @url.include?(' ')
+    @errors << "You have provided an invalid url."
+  end
+
+  if @description.length < 20
+    @errors << "You have provided an invalid description."
+  end
+
+  if @errors.length == 0
+    CSV.open('articles.csv', 'a', headers:true) do |csv|
+      csv << [@title, @url, @description]
+    end
+    erb :new
+
+    redirect '/articles'
+  else
+    erb :new
+  end
+end
+
+get '/articles/new' do
+  erb :new
+end
