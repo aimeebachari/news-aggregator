60e4c7d462f8192b166455532e290004f4df86be
Aimee Bachari <aimeebachari@gmail.com>
2016-11-30 19:54:10 -0500

    moved logic to the server

--- server.rb
+++ server.rb
@@ -5,32 +5,35 @@ require 'csv'
 set :bind, '0.0.0.0'
 
 get '/articles' do
+  @articles = []
+  CSV.foreach('articles.csv', headers:true) do |row|
+    @articles << row.to_h
+  end
   erb :articlelist
 end
 
 post '/articles/new' do
-  @title = params[:title]
-  @url = params[:url]
-  @description = params[:description]
+  title = params[:title]
+  url = params[:url]
+  description = params[:description]
   @errors = []
 
-  if @title == ""
+  if title == ""
     @errors << "You have provided an invalid title."
   end
 
-  if !@url.include?(".") || @url.include?(' ')
+  if !url.include?(".") || url.include?(' ')
     @errors << "You have provided an invalid url."
   end
 
-  if @description.length < 20
+  if description.length < 20
     @errors << "You have provided an invalid description."
   end
 
   if @errors.length == 0
     CSV.open('articles.csv', 'a', headers:true) do |csv|
-      csv << [@title, @url, @description]
+      csv << [title, url, description]
     end
-    erb :new
 
     redirect '/articles'
   else
